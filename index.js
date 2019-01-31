a47a0468364e27db7d4220e75956ee6250c0f40f
alexwhite116 <alexwhite116@users.noreply.github.com>
2019-01-31 02:23:03 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 "use strict";
 var app = require('./app.js');
 var server_port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+var server_ip_address = 'https://battleshipsgslw37.herokuapp.com'
 
 app.listen(server_port, server_ip_address, function(){ 
   console.log("port: " + server_port + ", ip: " + server_ip_address);
