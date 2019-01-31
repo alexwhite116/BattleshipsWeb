1c49985942555fa3cac00b046eaaa28c034bb9cb
alexwhite116 <alexwhite116@users.noreply.github.com>
2019-01-31 01:46:41 +0000

    Update index.js

--- index.js
+++ index.js
@@ -3,4 +3,6 @@ var app = require('./app.js');
 var server_port = process.env.OPENSHIFT_NODEJS_PORT
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP
 
-app.listen(server_port, server_ip_address);
+app.listen(server_port, server_ip_address, function(){ 
+  console.log("port: " + server_port + ", ip: " + server_ip_address);
+});
