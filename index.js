8c99658b2ae5c36068bdb5a4bc2918143f495781
alexwhite116 <alexwhite116@users.noreply.github.com>
2019-01-31 01:55:12 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 "use strict";
 var app = require('./app.js');
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+var server_ip_address = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
 app.listen(server_port, server_ip_address, function(){ 
   console.log("port: " + server_port + ", ip: " + server_ip_address);
